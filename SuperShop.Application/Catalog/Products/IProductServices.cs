de99f2faff6431583310eab2ae9e9a0f1030f9ce
VietPhong280120 <1851061519@e.tlu.edu.vn>
2021-09-09 21:34:38 +0700

    Crud Product

---
+++ SuperShop.Application/Catalog/Products/IProductServices.cs
@@ -0,0 +1,27 @@
+﻿using SuperShop.ViewModels.Catalog.Common;
+using SuperShop.ViewModels.Catalog.Products;
+using System.Threading.Tasks;
+
+namespace SuperShop.Application.Catalog.Products
+{
+    public interface IProductServices
+    {
+        Task<int> CreateProduct(ProductCreateRequest request);
+
+        Task<int> DeleteProduct(int productId);
+
+        Task<int> UpdateProduct(ProductUpdateRequest request);
+
+        Task<ProductVm> GetById(int productId, string languageId);
+
+        Task<PageResult<ProductVm>> GetAllPaging(GetProductPagingRequest request);
+
+        Task<PageResult<ProductVm>> GetByCategoryId(string languageId, GetProductPagingRequest request);
+
+        Task<int> AddImage(int productId, ProductImageCreateRequest request);
+
+        Task<int> UpdateImage(int imageId, ProductImageUpdateRequest request);
+
+        Task<int> RemoveImage(int imageId);
+    }
+}
