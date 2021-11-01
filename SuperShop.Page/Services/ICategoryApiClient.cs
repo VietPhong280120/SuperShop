ed212b7489e133c44fda28ef304976508f7b642a
VietPhong280120 <1851061519@e.tlu.edu.vn>
2021-11-01 20:08:49 +0700

    Manage webportal

---
+++ SuperShop.Page/Services/ICategoryApiClient.cs
@@ -0,0 +1,15 @@
+﻿using SuperShop.ViewModels.Catalog.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Page.Services
+{
+    public interface ICategoryApiClient
+    {
+        Task<List<CategoryVm>> GetAll(string languageid);
+
+        Task<CategoryVm> GetById(int id, string languageid);
+    }
+}
